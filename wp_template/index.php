240d763de060a478d684ff2830aec173d79556fc
Jonathan Bennett <jbennett@ccistudios.com>
2011-10-31 10:27:44 -0400

    Removed accidental commit (wordpress template)

--- wp_template/index.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-	<div id="content">
-		<?php get_template_part('loop', 'index'); ?>
-		<div class="clear"></div>
-	</div>
-<?php get_footer(); ?>
--- wp_template/page.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-	<div id="content">
-		<?php get_template_part('loop', 'page')?>
-		<div class="clear"></div>
-	</div>
-<?php get_footer(); ?>
