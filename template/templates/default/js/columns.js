ce4e07f2351459fca20d689b2b9a40913f6b145a
Jonathan Bennett <jbennett@ccistudios.com>
2011-11-09 16:12:24 -0500

    Added equal height columns

--- template/templates/default/js/columns.js
+++ template/templates/default/js/columns.js
@@ -1,1 +1,35 @@
 /* columns */
+var CCI = CCI || {};
+CCI.Columns = new Class({
+
+	container: null,
+	selector: null,
+	columns: null,
+	height: null,
+	
+	offset: null,
+
+	initialize: function(container, selector) {
+		this.container = container;
+		this.selector = selector;
+		this.columns = this.container.getElements(this.selector);
+		
+		this.offset = 52;
+		this.height = 0;
+		
+		for (i = this.columns.length - 1; i >= 0; i--) {
+			height = this.columns[i].getSize().y - this.offset;
+			console.log(height)
+			if (height > this.height) {
+				this.height = height;
+			}
+		}
+		
+		this.columns.setStyle('height', this.height);
+	}
+	
+});
+
+window.addEvent('load', function () {
+	c = new CCI.Columns($('bottom'), '.moduletable');
+});
