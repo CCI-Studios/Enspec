5e786dbdd1b4a2c4f77c214310f180b756fdf08b
Matt Elliott <melliott@ccistudios.com>
2011-11-18 13:15:38 -0500

    added rollover.js to index

--- template/templates/ENSPEC/index.php
+++ template/templates/ENSPEC/index.php
@@ -127,6 +127,7 @@ $analytics = "UA-XXXXX-X"; // FIXME Update to client ID
 		<script src="/templates/<?= $this->template ?>/js/columns.js"></script>
 		<script src="/templates/<?= $this->template ?>/js/dropmenu.js"></script>
 		<script src="/templates/<?= $this->template ?>/js/html5.js"></script>
+		<script src="/templates/<?= $this->template ?>/js/rollover.js"></script>
 	<?php else: ?>
 		<script>
 			var _gaq=[["_setAccount","<?php echo $analytics?>"],["_trackPageview"]];
